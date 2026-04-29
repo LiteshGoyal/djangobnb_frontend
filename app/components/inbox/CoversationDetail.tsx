aaed253c384bcf0730e18e116ab629e10df3c198
Sameer <liteshgoyal55@gmail.com>
2026-04-30 00:17:26 +0530

    Updated url in socket

--- app/components/inbox/CoversationDetail.tsx
+++ app/components/inbox/CoversationDetail.tsx
@@ -31,7 +31,7 @@ const CoversationDetail: React.FC<ConversationDetailProps> = ({
   const otherUser = conversation.users?.find((user) => user.id != userId);
 
   const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
-    `ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`,
+    `ws://${process.env.NEXT_PUBLIC_API_HOST}/ws/${conversation.id}/?token=${token}`,
     {
       share: false,
       shouldReconnect: () => true,
